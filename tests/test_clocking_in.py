d01d89d72924f6ded66b8f38119c2aaa037700c1
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-06-25 14:26:18 +0300

    Adjust test data for clocking in

--- tests/test_clocking_in.py
+++ tests/test_clocking_in.py
@@ -13,18 +13,18 @@ def test_clocking_in_records_start_time():
     repository = FakeEventRepository()
     use_case = ClockInUseCase(repository)
 
-    use_case.clock_in(datetime(2022, 5, 22, 19, 50))
+    use_case.clock_in(datetime(2022, 5, 22, 8, 30))
 
     assert repository.inserted_event == Event(
-        timestamp=datetime(2022, 5, 22, 19, 50),
-        type=EventType.IN
+        datetime(2022, 5, 22, 8, 30),
+        EventType.IN
     )
 
 
 def test_clocking_in_raises_an_exception_if_already_clocked_in():
-    last_event = Event(datetime(2022, 5, 22, 19, 50), EventType.IN)
+    last_event = Event(datetime(2022, 5, 22, 8, 30), EventType.IN)
     repository = FakeEventRepository(last_event)
     use_case = ClockInUseCase(repository)
 
     with raises(AlreadyClockedInError):
-        use_case.clock_in(datetime(2022, 5, 22, 19, 51))
+        use_case.clock_in(datetime(2022, 5, 22, 8, 31))

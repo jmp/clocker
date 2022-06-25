2dcc52f769f033e9c083fc72efc87eb21044d0f3
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-06-25 17:13:00 +0300

    Use unique file for each database

--- tests/test_sqlite_event_repository.py
+++ tests/test_sqlite_event_repository.py
@@ -5,23 +5,22 @@ from clocker.event import Event, EventType
 from clocker.repositories.sqlite_event_repository import SQLiteEventRepository
 
 
-SHARED_SQLITE_DATABASE = "file::memory:?cache=shared"
-
-
 def test_inserting_an_event_adds_a_new_row():
+    db_file = "file:insert?cache=shared&mode=memory"
     event = Event(datetime(2022, 5, 22, 8, 30, tzinfo=timezone.utc), EventType.IN)
-    repository = SQLiteEventRepository(SHARED_SQLITE_DATABASE)
+    repository = SQLiteEventRepository(db_file)
     repository.insert_event(event)
 
-    with connect(SHARED_SQLITE_DATABASE) as connection:
+    with connect(db_file) as connection:
         row = connection.execute("SELECT * FROM events").fetchone()
         assert row == ("2022-05-22 08:30:00+00:00", 0)
 
 
 def test_getting_last_event_fetches_the_last_row():
-    repository = SQLiteEventRepository(SHARED_SQLITE_DATABASE)
+    db_file = "file:getlast?cache=shared&mode=memory"
+    repository = SQLiteEventRepository(db_file)
 
-    with connect(SHARED_SQLITE_DATABASE) as connection:
+    with connect(db_file) as connection:
         connection.execute(
             "INSERT INTO events (timestamp, type) VALUES (?, ?), (?, ?)",
             (
@@ -38,7 +37,7 @@ def test_getting_last_event_fetches_the_last_row():
 
 
 def test_getting_last_event_returns_none_there_are_no_rows():
-    repository = SQLiteEventRepository(SHARED_SQLITE_DATABASE)
+    repository = SQLiteEventRepository(":memory:")
 
     last_event = repository.get_last_event()
 

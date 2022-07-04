f76b6ea866e2ee70d572d24474a2edd05efa1499
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-04 23:16:46 +0300

    Add a test for clocking in when clocked out

--- tests/test_clocking_in.py
+++ tests/test_clocking_in.py
@@ -10,13 +10,23 @@ from clocker.use_cases import ClockInUseCase
 from .mocks import MockEventRepository
 
 
-def test_clocking_in_records_an_in_event():
+def test_clocking_in_records_an_in_event_if_there_are_no_events():
     repository = MockEventRepository()
     use_case = ClockInUseCase(repository)
 
     use_case.clock_in(datetime(2022, 5, 22, 8, 30))
 
     assert repository.inserted_event == Event(datetime(2022, 5, 22, 8, 30), Action.IN)
+
+
+def test_clocking_in_records_an_in_event_if_clocked_out():
+    last_event = Event(datetime(2022, 5, 22, 16, 45), Action.OUT)
+    repository = MockEventRepository(last_event)
+    use_case = ClockInUseCase(repository)
+
+    use_case.clock_in(datetime(2022, 5, 23, 6, 15))
+
+    assert repository.inserted_event == Event(datetime(2022, 5, 23, 6, 15), Action.IN)
 
 
 def test_clocking_in_raises_an_exception_if_already_clocked_in():

e1566a6f2a1d44882c16fa9ab95f4fdda68558b1
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-07 00:17:42 +0300

    Simplify clocking in with type check

--- clocker/use_cases/clock_in.py
+++ clocker/use_cases/clock_in.py
@@ -1,6 +1,5 @@
 from ..errors import NotClockedOutError
 from ..event import InEvent
-from ..action import Action
 from ..repositories.event_repository import EventRepository
 from ..timestamp import Timestamp
 
@@ -11,7 +10,7 @@ class ClockInUseCase:
 
     def clock_in(self, timestamp: Timestamp):
         last_event = self._repository.get_last_event()
-        if last_event is not None and last_event.action == Action.IN:
+        if isinstance(last_event, InEvent):
             raise NotClockedOutError()
 
         self._repository.insert_event(InEvent(timestamp))

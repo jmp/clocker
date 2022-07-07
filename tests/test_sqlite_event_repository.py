a2274344b51d565a66226fd0648d55725884bfca
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-07 22:05:30 +0300

    Use fixture to make tests easier to read

--- tests/test_sqlite_event_repository.py
+++ tests/test_sqlite_event_repository.py
@@ -5,39 +5,26 @@ from clocker.repositories import SQLiteEventRepository
 from clocker.timestamp import Timestamp
 
 
-def test_inserting_an_event_adds_a_new_row():
-    db_file = "file:insert?cache=shared&mode=memory"
+def test_inserting_an_event_adds_a_new_row(database):
     event = InEvent(Timestamp("2022-05-22 11:30:00+03:00"))
-    repository = SQLiteEventRepository(db_file)
+    repository = SQLiteEventRepository(database.file)
+
     repository.insert_event(event)
 
-    with connect(db_file, uri=True) as connection:
-        row = connection.execute("SELECT * FROM events").fetchone()
-        assert row == ("2022-05-22 08:30:00", "IN")
+    assert database.events() == [("2022-05-22 08:30:00", "IN")]
 
 
-def test_getting_last_event_fetches_the_last_row():
-    db_file = "file:getlast?cache=shared&mode=memory"
-    repository = SQLiteEventRepository(db_file)
+def test_getting_last_event_fetches_the_last_row(database):
+    repository = SQLiteEventRepository(database.file)
 
-    with connect(db_file, uri=True) as connection:
-        connection.execute(
-            "INSERT INTO events (timestamp, action) VALUES (?, ?), (?, ?)",
-            (
-                "2022-05-01 05:59:02", "IN",
-                "2022-05-01 16:30:45", "OUT",
-            ),
-        )
-        connection.commit()
+    database.insert_event("2022-05-01 05:59:02", "IN")
+    database.insert_event("2022-05-01 16:30:45", "OUT")
 
-    last_event = repository.get_last_event()
-    expected_event = OutEvent(Timestamp("2022-05-01 16:30:45"))
-
-    assert last_event == expected_event
+    assert repository.get_last_event() == OutEvent(Timestamp("2022-05-01 16:30:45"))
 
 
-def test_getting_last_event_returns_none_if_there_are_no_rows():
-    repository = SQLiteEventRepository(":memory:")
+def test_getting_last_event_returns_none_if_there_are_no_rows(database):
+    repository = SQLiteEventRepository(database.file)
 
     last_event = repository.get_last_event()
 

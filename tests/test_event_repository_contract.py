704d061e18e8e2ac83b621d6894badd98fc9e14a
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-08 22:46:11 +0300

    Rename mock to fake

--- tests/test_event_repository_contract.py
+++ tests/test_event_repository_contract.py
@@ -6,58 +6,58 @@ from .helpers import InMemoryEventRepository
 
 
 def test_get_last_event_returns_the_same_value_by_default():
-    mock_repository = InMemoryEventRepository()
+    fake_repository = InMemoryEventRepository()
     sqlite_repository = SQLiteEventRepository(":memory:")
 
-    mock_result = mock_repository.find_last()
+    fake_result = fake_repository.find_last()
     sqlite_result = sqlite_repository.find_last()
 
-    assert mock_result is sqlite_result is None
+    assert fake_result is sqlite_result is None
 
 
 def test_insert_event_inserts_a_single_in_event():
-    mock_repository = InMemoryEventRepository()
+    fake_repository = InMemoryEventRepository()
     sqlite_repository = SQLiteEventRepository(":memory:")
     event = InEvent(Timestamp("2022-01-02 08:15"))
 
-    mock_repository.save(event)
+    fake_repository.save(event)
     sqlite_repository.save(event)
 
-    mock_result = mock_repository.find_last()
+    fake_result = fake_repository.find_last()
     sqlite_result = sqlite_repository.find_last()
     expected_result = InEvent(Timestamp("2022-01-02 08:15"))
 
-    assert mock_result == sqlite_result == expected_result
+    assert fake_result == sqlite_result == expected_result
 
 
 def test_insert_event_inserts_a_single_out_event():
-    mock_repository = InMemoryEventRepository()
+    fake_repository = InMemoryEventRepository()
     sqlite_repository = SQLiteEventRepository(":memory:")
     event = OutEvent(Timestamp("2022-01-02 15:30"))
 
-    mock_repository.save(event)
+    fake_repository.save(event)
     sqlite_repository.save(event)
 
-    mock_result = mock_repository.find_last()
+    fake_result = fake_repository.find_last()
     sqlite_result = sqlite_repository.find_last()
     expected_result = OutEvent(Timestamp("2022-01-02 15:30"))
 
-    assert mock_result == sqlite_result == expected_result
+    assert fake_result == sqlite_result == expected_result
 
 
 def test_insert_multiple_events():
-    mock_repository = InMemoryEventRepository()
+    fake_repository = InMemoryEventRepository()
     sqlite_repository = SQLiteEventRepository(":memory:")
     in_event = InEvent(Timestamp("2022-01-02 08:15"))
     out_event = OutEvent(Timestamp("2022-01-02 16:30"))
 
-    mock_repository.save(in_event)
-    mock_repository.save(out_event)
+    fake_repository.save(in_event)
+    fake_repository.save(out_event)
     sqlite_repository.save(in_event)
     sqlite_repository.save(out_event)
 
-    mock_result = mock_repository.find_last()
+    fake_result = fake_repository.find_last()
     sqlite_result = sqlite_repository.find_last()
     expected_result = OutEvent(Timestamp("2022-01-02 16:30"))
 
-    assert mock_result == sqlite_result == expected_result
+    assert fake_result == sqlite_result == expected_result
